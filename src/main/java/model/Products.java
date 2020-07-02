dad9da4bf86004b2674bfed30dd8d5e44b686d1a
Pivotal <pivotal@Pivotals-MacBook-Pro.local>
2020-07-02 12:10:35 +0100

    first jpa models

--- src/main/java/model/Products.java
+++ src/main/java/model/Products.java
@@ -2,32 +2,41 @@ package model;
 
 import lombok.*;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 
 @Data
 @Entity
-@Table(name = "domains")
+@Table(name = "products")
 @Getter @Setter
-class Domains {
+class Products {
 
 	private @Id @GeneratedValue(strategy = GenerationType.AUTO) @Column(name = "id")Long id;
 	
 	@Column(name = "name")
 	private String name;
-
-	@OneToOne(mappedBy = "domains")
-	private Products product;
 	
-	Domains() {}
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "lead", referencedColumnName = "id")
+	private Users lead;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "domainid", referencedColumnName = "id")
+	private Domains domain;
 
-	Domains(String name) {
+	Products() {}
+
+	Products(String name, Users lead, Domains domain) {
 		this.name = name;
+		this.lead = lead;
+		this.domain = domain;
 	}
 }
--- src/main/java/model/Users.java
+++ src/main/java/model/Users.java
@@ -2,10 +2,12 @@ package model;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 
@@ -15,11 +17,20 @@ import javax.persistence.Table;
 @Getter @Setter
 class Users {
 
-	private @Id @GeneratedValue(strategy = GenerationType.AUTO) Long id;
+	private @Id @GeneratedValue(strategy = GenerationType.AUTO) @Column(name = "id") Long id;
+	
+	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "email")
 	private String email;
+	
+	@Column(name = "slack")
 	private String slack;
 
+	@OneToOne(mappedBy = "users")
+	private Products product;
+	
 	Users() {}
 
 	Users(String name, String email, String slack) {
